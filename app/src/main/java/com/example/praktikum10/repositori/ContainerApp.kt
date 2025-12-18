2d36535faa291813ee853275c45b86385e8ef576
halimbagusalhakim <halsat132@gmail.com>
2025-12-18 12:25:44 +0700

    mendefinisikan kelas Application yang menginisialisasi ContainerApp saat aplikasi dijalankan agar dependency dapat digunakan secara global di seluruh aplikasi.

--- app/src/main/java/com/example/praktikum10/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/praktikum10/repositori/ContainerApp.kt
@@ -44,6 +44,13 @@ class DefaultContainerApp : ContainerApp{
     override val repositoryDataSiswa: RepositoryDataSiswa by lazy {
         JaringanRepositoryDataSiswa(retrofitService) }
 }
+class AplikasiDataSiswa : Application() {
+    lateinit var container: ContainerApp
+    override  fun onCreate() {
+        super.onCreate()
+        this.container = DefaultContainerApp()
+    }
+}
 
 
 
