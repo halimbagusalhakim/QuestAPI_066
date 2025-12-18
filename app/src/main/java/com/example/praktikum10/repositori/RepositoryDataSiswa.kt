fa5a1afea9bbd45870e30b60530bf081c9df7632
halimbagusalhakim <halsat132@gmail.com>
2025-12-18 12:10:30 +0700

    implementasi RepositoryDataSiswa yang menggunakan ServiceApiSiswa untuk mengambil dan mengirim data siswa ke server melalui Retrofit secara asynchronous.

--- app/src/main/java/com/example/praktikum10/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/praktikum10/repositori/RepositoryDataSiswa.kt
@@ -1,5 +1,6 @@
 package com.example.praktikum10.repositori
 
+import com.example.praktikum10.apiservice.ServiceApiSiswa
 import com.example.praktikum10.modeldata.DataSiswa
 
 interface RepositoryDataSiswa {
@@ -12,3 +13,11 @@ interface RepositoryDataSiswa {
 }
 
 
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+): RepositoryDataSiswa {
+    override suspend fun getDataSiswa() : List<DataSiswa> = serviceApiSiswa
+        .getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2
+    .Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+}
