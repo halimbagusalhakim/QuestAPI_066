3bab3a1ed53dfbf1f11919ff147edfd887713bde
halimbagusalhakim <halsat132@gmail.com>
2025-12-18 12:36:04 +0700

    mendefinisikan HomeViewModel yang mengambil data siswa dari repository secara asynchronous menggunakan coroutine, lalu mengelola status UI (Loading, Success, Error) berdasarkan hasil proses pengambilan data.

--- app/src/main/java/com/example/praktikum10/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/praktikum10/viewmodel/HomeViewModel.kt
@@ -19,3 +19,27 @@ sealed interface StatusUiSiswa {
     object Loading : StatusUiSiswa
 }
 
+class HomeViewModel(private val repositoryDataSiswa : RepositoryDataSiswa): ViewModel() {
+    var listSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
+        private set
+
+    init{
+        loadSiswa()
+    }
+
+    fun loadSiswa() {
+        viewModelScope.launch {
+            listSiswa = StatusUiSiswa.Loading
+            listSiswa = try {
+                StatusUiSiswa.Success(repositoryDataSiswa
+                    .getDataSiswa())
+            }catch (e: IOException){
+                StatusUiSiswa.Error
+            }
+            catch(e: HttpException){
+                StatusUiSiswa.Error
+            }
+        }
+    }
+}
+
