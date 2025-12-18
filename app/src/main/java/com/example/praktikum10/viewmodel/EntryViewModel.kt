3049a0cb4e51376aec71f02428296f52dcd97b95
halimbagusalhakim <halsat132@gmail.com>
2025-12-18 12:50:33 +0700

    mendefinisikan EntryViewModel yang mengelola input data siswa, melakukan validasi form, memperbarui state UI, dan mengirim data siswa ke server melalui repository menggunakan Retrofit.

---
+++ app/src/main/java/com/example/praktikum10/viewmodel/EntryViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.praktikum10.viewmodel
+
+import android.view.View
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.praktikum10.modeldata.DetailSiswa
+import com.example.praktikum10.modeldata.UIStateSiswa
+import com.example.praktikum10.modeldata.toDataSiswa
+import com.example.praktikum10.repositori.RepositoryDataSiswa
+import retrofit2.Response
+
+class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel(){
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with( uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
+    suspend fun addSiswa() {
+        if (validasiInput()) {
+            val sip: Response<Void> = repositoryDataSiswa
+                .postDataSiswa(uiStateSiswa.detailSiswa.toDataSiswa())
+            if (sip.isSuccessful) {
+                println("Sukses tambah data : ${sip.message()}")
+            }else {
+                println("gagal tambah data: ${sip.errorBody()}")
+            }
+        }
+    }
+}
