79959d0f5185a705d9499d37881ebe845f1eed76
halimbagusalhakim <halsat132@gmail.com>
2025-12-25 22:26:25 +0700

    membuat var uiStateSiswa

--- app/src/main/java/com/example/praktikum10/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/praktikum10/viewmodel/EditViewModel.kt
@@ -1,9 +1,15 @@
 package com.example.praktikum10.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.example.praktikum10.modeldata.UIStateSiswa
 import com.example.praktikum10.repositori.RepositoryDataSiswa
 
 class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel(){
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
 
 }
