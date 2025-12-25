0101855346b6364c973bbd0af0258bfc2bd4b5b0
halimbagusalhakim <halsat132@gmail.com>
2025-12-25 22:25:45 +0700

    membuat class editviewmodel

--- app/src/main/java/com/example/praktikum10/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/praktikum10/viewmodel/EditViewModel.kt
@@ -1,4 +1,9 @@
 package com.example.praktikum10.viewmodel
 
-class EditViewModel {
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.praktikum10.repositori.RepositoryDataSiswa
+
+class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel(){
+
 }
