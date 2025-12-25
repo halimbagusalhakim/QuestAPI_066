ad59575ecda6c4f364694520c684ea203b8557b6
halimbagusalhakim <halsat132@gmail.com>
2025-12-25 22:27:33 +0700

    membuat var uiStateSiswamengambil ID siswa dari navigasi, memuat detail satu siswa dari repository secara asynchronous saat ViewModel dibuat, lalu mengelola dan memperbarui state UI serta validasi data ketika terjadi perubahan input.

--- app/src/main/java/com/example/praktikum10/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/praktikum10/viewmodel/EditViewModel.kt
@@ -5,11 +5,27 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.praktikum10.modeldata.DetailSiswa
 import com.example.praktikum10.modeldata.UIStateSiswa
+import com.example.praktikum10.modeldata.toUiStateSiswa
 import com.example.praktikum10.repositori.RepositoryDataSiswa
+import com.example.praktikum10.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
 
 class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel(){
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
 
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                .toUiStateSiswa(true)
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa){
+        uiStateSiswa = UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
 }
