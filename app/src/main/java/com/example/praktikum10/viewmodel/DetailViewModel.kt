4c5f29838391beecabb6089e9ef800eea57a1f5d
halimbagusalhakim <halsat132@gmail.com>
2025-12-25 22:29:49 +0700

    mendefinisikan status UI (loading, sukses, dan error) pada halaman detail siswa serta mengelola pengambilan satu data siswa berdasarkan ID di ViewModel saat pertama kali dibuat.

--- app/src/main/java/com/example/praktikum10/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/praktikum10/viewmodel/DetailViewModel.kt
@@ -1,4 +1,22 @@
 package com.example.praktikum10.viewmodel
 
-class DetailViewModel {
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.praktikum10.modeldata.DataSiswa
+import com.example.praktikum10.repositori.RepositoryDataSiswa
+import com.example.praktikum10.uicontroller.route.DestinasiDetail
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: DataSiswa) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
 }
+class DetailViewModel (savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel(){
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+
+    init {
+        getSatuSiswa()
+    }
