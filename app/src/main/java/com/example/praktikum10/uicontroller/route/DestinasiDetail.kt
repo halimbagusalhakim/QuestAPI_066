6282de6bd239cf35a57a37478a4af922ae4f3e42
halimbagusalhakim <halsat132@gmail.com>
2025-12-25 22:20:08 +0700

    Mendefinisikan argumen ID siswa yang dikirim saat navigasi

--- app/src/main/java/com/example/praktikum10/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/praktikum10/uicontroller/route/DestinasiDetail.kt
@@ -5,6 +5,6 @@ import com.example.praktikum10.R
 object DestinasiDetail: DestinasiNavigasi {
     override val route = "detail_siswa"
     override val titleRes = R.string.detail_siswa
-
+    const val itemIdArg = "idSiswa"
 
 }
