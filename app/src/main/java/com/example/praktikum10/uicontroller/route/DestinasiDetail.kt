8fd6f823bc78ef8b01e7f24378e9bc510e5ee5bf
halimbagusalhakim <halsat132@gmail.com>
2025-12-25 22:19:33 +0700

    menentukan judul haalaman

--- app/src/main/java/com/example/praktikum10/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/praktikum10/uicontroller/route/DestinasiDetail.kt
@@ -1,6 +1,10 @@
 package com.example.praktikum10.uicontroller.route
 
+import com.example.praktikum10.R
+
 object DestinasiDetail: DestinasiNavigasi {
     override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+
 
 }
