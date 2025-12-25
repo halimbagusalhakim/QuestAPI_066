03cea60f57d30929fa52b4f50618076b80961b32
halimbagusalhakim <halsat132@gmail.com>
2025-12-25 22:20:39 +0700

    Membuat rute navigasi lengkap yang menerima parameter ID siswa.

--- app/src/main/java/com/example/praktikum10/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/praktikum10/uicontroller/route/DestinasiDetail.kt
@@ -6,5 +6,6 @@ object DestinasiDetail: DestinasiNavigasi {
     override val route = "detail_siswa"
     override val titleRes = R.string.detail_siswa
     const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 
 }
